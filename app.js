d72db4658727d4cfac1835ab688e73f0cfe8b7f1
KD-69 <78873138+KD-69@users.noreply.github.com>
2021-10-30 22:51:29 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+// Show and Hide section
+
+function showMenu(){
+    document.getElementById("navbar").style.left = "0";
+}
+
+function hideMenu(){
+    document.getElementById("navbar").style.left = "-250px"
+}
+
